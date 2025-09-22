ec6ac3fdc23e51fe50c6c63a8fa156f8c9bdd846
gallant4649 <gallantfathurrahman@gmail.com>
2025-09-22 15:34:43 +0700

    Update title and enhance layout in PersonaCompendiumApp; remove unused styles and config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="container">
-      <div className="max-w-full mx-auto p-3 rounded-b-lg">
+      <div className="max-w-full w-85 mx-auto p-3 rounded-b-lg">
         <img src="./src/assets/fontbolt.png" alt="Logo" className="mx-auto" />
       </div>
     </div>
